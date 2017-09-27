10acadcfec183e9e210b6e2ce44c4bee8caca7bd
Stuart Haidon <serayen.sh@gmail.com>
2017-09-27 20:30:42 +0100

    Changed test to indicate key type.

--- src/iter/group_by_key.rs
+++ src/iter/group_by_key.rs
@@ -38,11 +38,11 @@ mod tests {
     #[test]
     fn group() {
         let ints = 1_u32..10;
-        let odd_even = ints.group_by_key(|i| i%2);
+        let odd_even = ints.group_by_key(|i| i%2 == 0);
 
         let mut expected = HashMap::new();
-        expected.insert(0, vec![2,4,6,8]);
-        expected.insert(1, vec![1,3,5,7,9]);
+        expected.insert(true, vec![2,4,6,8]);
+        expected.insert(false, vec![1,3,5,7,9]);
 
         assert_eq!(odd_even, expected);
     }
